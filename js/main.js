f7b463ead93c36af3fc83c5403b3508eae65590b
Nchaos <chaosnick@hotmail.com>
2015-03-24 16:40:40 -0500

    forgot what i did lol

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
+<!-- Add jQuery to your website if you dont have it already -->
+
+<!-- JAVASCRIPT to clear search text when the field is clicked -->
+<script type="text/javascript">
+$(function() {
+	$("#tfq2b").click(function() {
+		if ($("#tfq2b").val() == "Search our website"){
+			$("#tfq2b").val(""); 
+		}
+	});
+});
+
