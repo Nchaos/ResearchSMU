589d43f774e23e88954fb22d5d3873158719f81f
Nchaos <chaosnick@hotmail.com>
2015-03-24 20:11:32 -0500

    not sure what i did here lol

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-
-<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
-<!-- Add jQuery to your website if you dont have it already -->
-
 <!-- JAVASCRIPT to clear search text when the field is clicked -->
 <script type="text/javascript">
 $(function() {
