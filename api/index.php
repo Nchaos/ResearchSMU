5f067c97cc165c2edc03c26a167d3c1b30d6cc91
harshnormal <prestondeas15@gmail.com>
2015-04-08 18:56:58 -0500

    Added analytics!

--- api/index.php
+++ api/index.php
@@ -313,6 +313,7 @@
 		$deptId = $_SESSION['deptId'];
 		$check = $_POST['check'];
 		$name = $_POST['name'];
+		$description = $_POST['desc'];
 		$dateStart = $_POST['dateStart'];
 		$dateEnd = $_POST['dateEnd'];
 		$numPositions = $_POST['numPositions'];
@@ -320,18 +321,22 @@
 		$workStudy = $_POST['workStudy'];
 		$graduate = $_POST['graduate'];
 		$undergraduate = $_POST['undergraduate'];
+		$todayDate = date("Y-m-d");
 		
 		if($name === "" || $dateStart === "" || $dateEnd === "" || $numPositions === "")
 			die(json_encode(array('ERROR' => 'Received blank parameters from creation page')));
 		else{
-			$dupCheck = $mysqli->query("SELECT TOP 1 researchOp_ID FROM ResearchOp WHERE user_ID='$userId' AND name='$name' AND dateStart='$dateStart' AND dateEnd='$dateEnd' AND num_Positions='$numPositions'");
+			$dupCheck = $mysqli->query("SELECT TOP 1 researchOp_ID FROM ResearchOp WHERE user_ID='$userId' AND name='$name' AND dateStart='$dateStart' AND num_Positions='$numPositions'");
 			$checkResults = $dupCheck->fetch_assoc();
 			
 			if(!($checkResults === NULL))
 				die(json_encode(array('ERROR' => 'Research Opportunity already exists')));
 			else{
-				$insertROP = $mysqli->query("INSERT INTO ResearchOp (user_ID, inst_ID, dept_ID, name, dateStart, dateEnd, num_Positions, paid, work_study, graduate, undergraduate)
-					VALUES ('$userId', '$instId', '$deptId', '$name', '$dateStart', '$dateEnd', '$numPositions', '$paid', '$workStudy', '$graduate', '$undergraduate')");
+				$insertROP = $mysqli->query("INSERT INTO ResearchOp (user_ID, inst_ID, dept_ID, dateCreated, 
+					name, description, startDate, endDate, numPositions, paid, workStudy, acceptsUndergrad, 
+					acceptsGrad) 
+					VALUES ('$userId', '$instId', '$deptId', '$dateCreated', '$name', '$dateStart', '$dateEnd', 
+					'$numPositions', '$paid', '$workStudy', '$graduate', '$undergraduate')");
 				die(json_encode(array('Status' => 'Success')));
 			}
 		}
