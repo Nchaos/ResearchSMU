6a6cd7db6b18be1c0a93c03c5e4628a2e472f928
Preston H. Deason <prestondeas15@gmail.com>
2015-04-07 19:31:55 -0500

    Added echos

--- api/index.php
+++ api/index.php
@@ -239,41 +239,50 @@ $app->post('/logout', function() {
 //							Register							//
 //==============================================================//
 $app->post('/createAccount', function(){
+	echo "Registering user...\n";
 	global $mysqli;
 	$check = $_POST['studentOrFaculty'];
 	$firstName = $_POST['firstName'];
 	$lastName = $_POST['lastName'];
 	$email = $_POST['email'];
 	$userId = '';
+	echo "Retrieved parameters...\n";
 	
 	if($firstName === "" || $lastName === "" || $email === "" || $password === "")
 		die(json_encode(array('ERROR' => 'Received blank parameters from registration')));
 	else{
+		echo "Checking for duplicate... ";
 		$dupCheck = $mysqli->query("SELECT email FROM Users WHERE email='$email'");
 		$checkResults = $dupCheck->fetch_assoc();
 		if(!($checkResults === NULL))
 			die(json_encode(array('ERROR' => 'User already exists')));
 		else{
+			echo "None found!\n";
 			$saltValue = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
 			$hashedPassword = create_hash($password, $saltValue);
-
+			
+			echo "Inserting user... ";
 			$insertUser = $mysqli->query("INSERT INTO Users (fName, lName, email, saltValue) VALUES ('$firstName', '$lastName', '$email', '$saltValue')");
 			$userId = $mysqli->query("SELECT user_ID FROM Users where email='$email'");
 			$insertPassword = $mysqli->query("INSERT INTO Password (user_ID, password) VALUES ('$userId', '$hashedPassword')");
+			
+			if($check === "Student"){
+				echo "Student\n";
+				$instId = $_POST['instId'];
+				$major = $_POST['major'];
+				$grad = $_POST['grad'];
+				$insertStudent = $mysqli->query("INSERT INTO Student (user_ID, inst_ID, dept_ID, graduateStudent) VALUES ('$userId', '$instId', '$major', '$grad')");
+			}
+			elseif($check === "Faculty"){
+				echo "Faulty\n";
+				$instId = $_POST['instId'];
+				$deptId = $_POST['deptId'];
+				$insertFaculty = $mysqli->query("INSERT INTO Faculty (user_ID, inst_ID, dept_ID) VALUES ('$userId', '$instId', '$deptId')");
+			}
 		}
 	}
 	
-	if($check === "Student"){
-		$instId = $_POST['instId'];
-		$major = $_POST['major'];
-		$grad = $_POST['grad'];
-		$insertStudent = $mysqli->query("INSERT INTO Student (user_ID, inst_ID, dept_ID, graduateStudent) VALUES ('$userId', '$instId', '$major', '$grad')");
-	}
-	elseif($check === "Faculty"){
-		$instId = $_POST['instId'];
-		$deptId = $_POST['deptId'];
-		$insertFaculty = $mysqli->query("INSERT INTO Faculty (user_ID, inst_ID, dept_ID) VALUES ('$userId', '$instId', '$deptId')");
-	}
+
 });
 
 //==============================================================//
