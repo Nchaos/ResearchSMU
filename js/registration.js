a55f200137d245dc6479a8ca26068c9203c0eb68
Nchaos <chaosnick@hotmail.com>
2015-04-14 19:33:06 -0500

    what

--- js/registration.js
+++ js/registration.js
@@ -65,19 +65,4 @@
   }
 
 
-  function checkOPForm(form)
-  {
-    if(form.OPtitle.value == "") {
-      alert("Error: Need a title!");
-      form.Optitle.focus();
-      return false;
-    }
-    re = /^\w+$/;
-    if(!re.test(form.OPtitle.value)) {
-      alert("Error: Need a title!");
-      form.OPtitle.focus();
-      return false;
-    }
-    return true;
-  }
 
