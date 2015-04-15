737c72293d0c4f581075c917ae396c0a9dc2fbcc
mdevitt04 <mdevitt@smu.edu>
2015-04-15 18:34:33 -0500

    I think this should make the checkbox checking work

--- js/results.js
+++ js/results.js
@@ -4,40 +4,49 @@ $(document).ready(function() {
 });
 
 
-var CheckboxHandler = new Object();
-
-
-CheckboxHandler.isChecked = function (checkboxObj) {
-  return(checkboxObj.checked == true);
-};
-
-function getFormValues(oForm, skip_elements) {
-   
-  var elements = oForm.elements; 
-  var data = [];
-  var element_value = null;
-
-  for(var i = 0; i < elements.length; i++) {
-     
-	var field_type = elements[i].type.toLowerCase();
-	var element_name = elements[i].getAttribute("name");
-	
-	if(!skip_elements.length ||  !skip_elements.in_array(element_name)) {
-	
-	switch(field_type) {
-		case "checkbox":
-			
-			element_value = CheckboxHandler.isChecked(elements[i]);
-			data.push(element_name + ': ' + element_value);
-			break;
-
-			default: 
-			break;
-	}
-        }
-  }	
-  var rData = JSON.strigify($)
-  return data; 
+function getCheckedBoxes(checkboxName) {
+	var checkboxes = document.querySelectorAll('input[class="' + checkboxName + '"]:checked'), values = [];
+	Array.prototype.forEach.call(checkboxes, function(el)	{
+		values.push(el.value);
+	});
+	return values;
 }
+
+
+// var CheckboxHandler = new Object();
+
+
+// CheckboxHandler.isChecked = function (checkboxObj) {
+//   return(checkboxObj.checked == true);
+// };
+
+// function getFormValues(oForm, skip_elements) {
+   
+//   var elements = oForm.elements; 
+//   var data = [];
+//   var element_value = null;
+
+//   for(var i = 0; i < elements.length; i++) {
+     
+// 	var field_type = elements[i].type.toLowerCase();
+// 	var element_name = elements[i].getAttribute("name");
+	
+// 	if(!skip_elements.length ||  !skip_elements.in_array(element_name)) {
+	
+// 	switch(field_type) {
+// 		case "checkbox":
+			
+// 			element_value = CheckboxHandler.isChecked(elements[i]);
+// 			data.push(element_name + ': ' + element_value);
+// 			break;
+
+// 			default: 
+// 			break;
+// 	}
+//         }
+//   }	
+//   var rData = JSON.strigify($)
+//   return data; 
+// }
 
 
