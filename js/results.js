33f5ac59f41c42679c7359eb86c88214f0a73cfb
Nchaos <chaosnick@hotmail.com>
2015-05-04 16:26:47 -0500

    nothing

--- js/results.js
+++ js/results.js
@@ -110,10 +110,11 @@ function format ( d ) {
             '</tr>'+
             '<tr>'+
                 '<td>Description:</td>'+
-                '<td>'+d.description+'</td>'+
+                '<td>'+d.descript+'</td>'+
             '</tr>'+
             '<tr>'+
                 '<td></td>'+
+                //<td><button type="button" class="btn btn-primary">Primary</button></td>
                 '<td><button>Apply</button></td>'+
             '</tr>'+
         '</table>'+
@@ -192,7 +193,7 @@ function instReloadData (num){
 }
 
 function tabInstitutionHandler(num) {
-  	console.log("Inst handler: " + num);
+  	//console.log("Inst handler: " + num);
   	var instValue = num; 
   	var searchString = {"institution" : instValue};
 
