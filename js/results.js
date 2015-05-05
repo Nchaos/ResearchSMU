9eeea736690d8555291bef7d6b4c4e67f3a40e23
Nchaos <chaosnick@hotmail.com>
2015-05-05 18:17:37 -0500

    working on filter by checkboxes

--- js/results.js
+++ js/results.js
@@ -1,53 +1,3 @@
-// $(document).ready(function() {
-    
-
-// });
-
-
-function getCheckedBoxes(checkboxName) {
-	var checkboxes = document.querySelectorAll('input[class="' + checkboxName + '"]:checked'), values = [];
-	Array.prototype.forEach.call(checkboxes, function(el)	{
-		values.push(el.value);
-	});
-	return values;
-}
-
-
-var CheckboxHandler = new Object();
-
-
-CheckboxHandler.isChecked = function (checkboxObj) {
-  return(checkboxObj.checked == true);
-};
-
-function getFormValues(oForm, skip_elements) {
-   
-  var elements = oForm.elements; 
-  var data = [];
-  var element_value = null;
-
-  for(var i = 0; i < elements.length; i++) {
-     
-	var field_type = elements[i].type.toLowerCase();
-	var element_name = elements[i].getAttribute("name");
-	
-	if(!skip_elements.length ||  !skip_elements.in_array(element_name)) {
-	
-	switch(field_type) {
-		case "checkbox":
-			element_value = CheckboxHandler.isChecked(elements[i]);
-			data.push(element_name + ': ' + element_value);
-			break;
-
-			default: 
-			break;
-	}
-        }
-  }	
-  var rData = JSON.strigify($);
-  return data; 
-}
-
 
 // function format ( d ) {
 // 	console.log(d);
@@ -274,7 +224,70 @@ function autocomplete() {
 });	
 
 
-function filter(filter) {
-    window.alert("It Works!!!!!!! Filter: "+filter)
+function filter() {
+	getCheckedBoxes();
+	console.log(CheckboxHandler);
+    //window.alert("It Works!!!!!!! Filter: " + CheckboxHandler);
 }
+
+
+// Object to store a json on all the depts selected
+var CheckboxHandler = new Object();
+CheckboxHandler.dept = [];
+
+function getCheckedBoxes() {
+	CheckboxHandler.dept = [];
+	var checkboxes = document.getElementsByClassName('Dept');
+	//alert(checkboxes.length);
+	for (var i =0; i < checkboxes.length; i++){
+		if (checkboxes[i].checked == true){
+			//alert("holy shit it works!");
+			CheckboxHandler.dept[i] = checkboxes[i].value;
+		}
+		// else{
+		// 	CheckboxHandler.dept[i] = null;
+		// }
+	}
+
+
+
+	// var checkboxes = document.querySelectorAll('input[class="' + checkboxName + '"]:checked'), values = [];
+	// Array.prototype.forEach.call(checkboxes, function(el)	{
+	// 	values.push(el.value);
+	// });
+	// return values;
+}
+
+
+// CheckboxHandler.isChecked = function (checkboxObj) {
+//   return(checkboxObj.checked == true);
+// };
+
+// function getFormValues(oForm, skip_elements) {
+   
+//   var elements = oForm.elements; 
+//   var data = [];
+//   var element_value = null;
+
+//   for(var i = 0; i < elements.length; i++) {
+     
+// 	var field_type = elements[i].type.toLowerCase();
+// 	var element_name = elements[i].getAttribute("name");
+	
+// 	if(!skip_elements.length ||  !skip_elements.in_array(element_name)) {
+	
+// 	switch(field_type) {
+// 		case "checkbox":
+// 			element_value = CheckboxHandler.isChecked(elements[i]);
+// 			data.push(element_name + ': ' + element_value);
+// 			break;
+
+// 			default: 
+// 			break;
+// 	}
+//         }
+//   }	
+//   var rData = JSON.strigify($);
+//   return data; 
+// }
 
