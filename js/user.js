f228ff97ae836f6efea7c0e8dd1359de1027b760
ChimichangasRock <amazingfanzy@gmail.com>
2015-05-08 04:35:18 -0500

    User page basics made

--- js/user.js
+++ js/user.js
@@ -17,37 +17,54 @@ function userInfo(){
 	
 	console.log("userInfo");
 	
-	document.getElementById("info").innerHTML = "Insert Info";
+
+	$.ajax({
+		type: "POST",
+		url: "api/index.php/userinfo",
+		dataType: "json",
+		success: function(data){
+			var value = JSON.parse(data);
+			console.log(value);
+			var email = value['email'];
+			var fname = value['firstname'];
+			var lname = value['lastname'];
+			var major = "CSE"; //value[''];
+			var status = value['userType'];
 	
+			var html_string1 = "<table><tr><td>Email: </td><td style='padding-left: 8em'>"+email+"</td></tr><tr><td>First Name: </td><td style='padding-left: 8em'>"+fname+"</td></tr><tr><td>Last Name: </td><td style='padding-left: 8em'>"+lname;
+			var html_string2 = "</td></tr><tr><td>Major: </td><td style='padding-left: 8em'>"+major+"</td></tr><tr><td>Grad or Undergrad: </td><td style='padding-left: 8em'>"+status+"</td></tr></table>";
 	
+			document.getElementById("info").innerHTML = html_string1 + html_string2;
+				
+		},
+		error: function(jqXHR, textStatus, errorThrown){alert(errorThrown);}
+	});
 	
-	
-	
-	
-	
-	
+	console.log("Done");
 }
 
 function editInfo() {
+	$("#info").css("display", 'none');
+	
 	var fname = document.createElement('div');
     fname.innerHTML = "First Name: <br><input type='text' name='fname'>";
-    document.getElementById(fname).appendChild(fname);
+    document.getElementById("newInfo").appendChild(fname);
     
     var lname = document.createElement('div');
     lname.innerHTML = "Last Name: <br><input type='text' name='lname'>";
-    document.getElementById(lname).appendChild(lname);
+    document.getElementById("newInfo").appendChild(lname);
     
     var pwd1 = document.createElement('div');
     pwd1.innerHTML = "Password: <br><input type='text' name='pwd'>";
-    document.getElementById(pwd1).appendChild(pwd1);
+    document.getElementById("newInfo").appendChild(pwd1);
     
     var pwd2 = document.createElement('div');
     pwd2.innerHTML = "Password Confirmation: <br><input type='text' name='confirm'>";
-    document.getElementById(pwd2).appendChild(pwd2);
+    document.getElementById("newInfo").appendChild(pwd2);
     
     var dept = document.createElement('div');
     dept.innerHTML = "Department: <br><input type='text' name='dept'>";
-    document.getElementById(dept).appendChild(dept);
+    document.getElementById("newInfo").appendChild(dept);
 }
 
 function uploadResume(){
