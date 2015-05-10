93ff4df1a44920bd17b0c020abfb5ac75d7998ab
Nchaos <chaosnick@hotmail.com>
2015-05-10 02:16:53 -0500

    fix #369

--- js/main.js
+++ js/main.js
@@ -89,13 +89,13 @@ $(document).ready(function() {
 });  
 
     <!-- //JAVASCRIPT to clear search text when the field is clicked -->
-$(function() {
-	$("#tfq2b").click(function() {
-		if ($("#tfq2b").val() == "Search our website"){
-			$("#tfq2b").val(""); 
-		}
-	});
-});
+// $(function() {
+// 	$("#tfq2b").click(function() {
+// 		if ($("#tfq2b").val() == "Search our website"){
+// 			$("#tfq2b").val(""); 
+// 		}
+// 	});
+// });
 
 (function($) {
 
